24a5ae2eb12eba095776c13623102aabe439567a
Jordan Marcus <jormarcus92@gmail.com>
2018-01-31 22:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function shout (string) {
+function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper (string) {
+function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string)
