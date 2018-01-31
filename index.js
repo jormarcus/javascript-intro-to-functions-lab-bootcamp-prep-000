d8bf5e39e8c5c88b2eefa54b5027aea399276eb4
Jordan Marcus <jormarcus92@gmail.com>
2018-01-31 22:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string !== string.toUpperCase()) {
-    return 'I can\'t hear you!'
-  } else if (string === 'I love you, Grandma.') {
+  if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
+  } else if (string !== string.toUpperCase()) {
+    return 'I can\'t hear you!' 
   } else {
     return 'YES INDEED!'
   }
